e7c1fbcfe0357b7b7d747b5968933ad62bd1070d
Seth Forte <sethforte1@gmail.com>
2025-04-27 09:57:22 -0500

    AZdding Exercise files

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -2,9 +2,41 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create a random number generator
+            var random = new Random();
+            var favoriteNumber = random.Next(1, 1000); // Generate a random number between 1 and 1000
+
+            Console.WriteLine("Try to guess the favorite number (between 1 and 1000):");
+
+            while (true)
+            {
+                Console.Write("Enter your guess: ");
+                var input = Console.ReadLine();
+
+                // Validate user input
+                if (int.TryParse(input, out int userGuess))
+                {
+                    if (userGuess == favoriteNumber)
+                    {
+                        Console.WriteLine("You guessed the correct number!");
+                        break;
+                    }
+                    else if (userGuess < favoriteNumber)
+                    {
+                        Console.WriteLine("Your guess is too low.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your guess is too high.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+            }
         }
     }
 }
--- SelectionStatementExercise/obj/Release/net9.0/SelectionStatementExercise.AssemblyInfo.cs
+++ SelectionStatementExercise/obj/Release/net9.0/SelectionStatementExercise.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("SelectionStatementExercise")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+289197ca83344713fd5e15f53b08b850916bb8ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("SelectionStatementExercise")]
